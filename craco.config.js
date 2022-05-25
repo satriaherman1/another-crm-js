8e74b88346994fc95e8cbc1778b80baaec0533df
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-25 22:24:31 +0700

    fix craco config

--- craco.config.js
+++ craco.config.js
@@ -1,15 +1,15 @@
 const path = require(`path`);
-const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
+// const WindiCSSWebpackPlugin = require("windicss-webpack-plugin");
 
 module.exports = {
   webpack: {
-    plugins: {
-      add: [
-        new WindiCSSWebpackPlugin({
-          virtualModulePath: "src",
-        }),
-      ],
-    },
+    // plugins: {
+    //   add: [
+    //     new WindiCSSWebpackPlugin({
+    //       virtualModulePath: "src",
+    //     }),
+    //   ],
+    // },
 
     alias: {
       "@src": path.resolve(__dirname, "src/"),
