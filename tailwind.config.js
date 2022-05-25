5854f30f3514992d866e0620733dae75876e64b0
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-25 22:27:11 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-const { defineConfig } = require("windicss/helpers");
-
-module.exports = defineConfig({
-  extract: {
-    include: ["**/*.{jsx,js,css,html}"],
-    exclude: ["node_modules", ".git", ".next"],
-  },
+module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         "crm-dark": "#141518",
+        "crm-gray-700": "#98A5CD",
         "crm-gray-600": "#AFAFAF",
         "crm-gray-500": "#C4C4C4",
+        "crm-gray-420": "#2E2F35",
         "crm-gray-400": "#2C2E34",
+        "crm-gray-370": "#4B4A4A",
         "crm-gray-350": "#404554",
+        "crm-gray-320": "#3A3F4F",
         "crm-gray-300": "#3C4254",
+        "crm-gray-280": "#23242c",
         "crm-gray-200": "#282C38",
+        "crm-gray-170": "#7C7F88",
         "crm-gray": "#757B8C",
         "crm-primary": "#0EC8CE",
         "crm-dark-400": "#333641",
@@ -24,8 +24,13 @@ module.exports = defineConfig({
         "crm-red": "#EB5757",
         "crm-yellow": "#FFB545",
         "crm-orange": "#F2994A",
+        "crm-green": "#30E0A1",
+        "crm-green-300": "#05850c",
+        "crm-green-800": "#2D493F",
+        "crm-yellow": "#FACB22",
+        "crm-red": "#EC3748",
       },
     },
   },
   plugins: [],
-});
+};
