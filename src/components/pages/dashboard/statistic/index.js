fddca16287fec1555b003ef134e5db659124e6aa
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-25 22:56:28 +0700

    remove email tab

--- src/components/pages/dashboard/statistic/index.js
+++ src/components/pages/dashboard/statistic/index.js
@@ -5,51 +5,47 @@ import { useState } from "react";
 import FilterTab from "@src/components/common/FilterTab";
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: "top",
-        },
-        // title: {
-        //   display: true,
-        //   text: "Chart.js Line Chart",
-        // },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
     },
+    // title: {
+    //   display: true,
+    //   text: "Chart.js Line Chart",
+    // },
+  },
 };
 const labels = ["Today", "Fri Apr 15", "Sat Apr 16", "Sun Apr 17", "Mon Apr 18", "Tue Apr 19", "Wed Apr 20", "Thu Apr 21"];
 export const data = {
-    labels,
-    datasets: [
-        {
-            label: "Dataset 1",
-            data: labels.map(() => faker.datatype.number({ min: -1, max: 1.5 })),
-            borderColor: "#0EC8CE",
-            backgroundColor: "#0EC8CE",
-        },
-        {
-            label: "Dataset 2",
-            data: labels.map(() => faker.datatype.number({ min: -1, max: 1.5 })),
-            borderColor: "#ffffff",
-            backgroundColor: "#ffffff",
-        },
-    ],
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: labels.map(() => faker.datatype.number({ min: -1, max: 1.5 })),
+      borderColor: "#0EC8CE",
+      backgroundColor: "#0EC8CE",
+    },
+    {
+      label: "Dataset 2",
+      data: labels.map(() => faker.datatype.number({ min: -1, max: 1.5 })),
+      borderColor: "#ffffff",
+      backgroundColor: "#ffffff",
+    },
+  ],
 };
 export default function DashboardStatistic() {
-    const [activeTab, setActiveTab] = useState("task");
-    const taskTabButton = [
-        {
-            key: "task",
-            onClick: () => setActiveTab("task"),
-            activeIndicator: activeTab,
-        },
-        {
-            key: "emails",
-            onClick: () => setActiveTab("emails"),
-            activeIndicator: activeTab,
-        },
-    ];
-    return (<section className="dashboard-statistic  bg-crm-dark-300 text-white w-full px-5 py-3 rounded-md h-[fit-content]">
-      <FilterTab tabButton={taskTabButton}/>
+  const [activeTab, setActiveTab] = useState("task");
+  const taskTabButton = [
+    {
+      key: "task",
+      onClick: () => setActiveTab("task"),
+      activeIndicator: activeTab,
+    },
+  ];
+  return (
+    <section className="dashboard-statistic  bg-crm-dark-300 text-white w-full px-5 py-3 rounded-md h-[fit-content]">
+      <FilterTab tabButton={taskTabButton} />
 
       <p className="text-white mt-7 font-semibold">Upcoming Task</p>
       <p className="text-white mt-7 text-[14px]">
@@ -75,7 +71,8 @@ export default function DashboardStatistic() {
       </div>
 
       <div className="w-full">
-        <Line options={options} data={data}/>
+        <Line options={options} data={data} />
       </div>
-    </section>);
+    </section>
+  );
 }
