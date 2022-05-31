43286226c8dac2b0cfd70ae5b7646e3649b20447
satriaherman1 <muhammadsatriaherman@gmail.com>
2022-05-31 21:03:15 +0700

    fix issue

--- src/components/plugin/task/index.js
+++ src/components/plugin/task/index.js
@@ -7,52 +7,56 @@ import useLocalData from "@src/utility/hooks/useLocalData";
 import { useState } from "react";
 import "./styles.scss";
 export default function PluginTask() {
-    const [activeFilterTab, setActiveFilterTab] = useState("task");
-    const filterTabList = [
-        {
-            activeIndicator: activeFilterTab,
-            key: "account",
-            onClick: () => setActiveFilterTab("account"),
-        },
-        {
-            activeIndicator: activeFilterTab,
-            key: "contacts",
-            onClick: () => setActiveFilterTab("contacts"),
-        },
-        {
-            activeIndicator: activeFilterTab,
-            key: "opportunities",
-            onClick: () => setActiveFilterTab("opportunities"),
-        },
-        {
-            activeIndicator: activeFilterTab,
-            key: "task",
-            onClick: () => setActiveFilterTab("task"),
-        },
-        {
-            activeIndicator: activeFilterTab,
-            key: "activity",
-            onClick: () => setActiveFilterTab("activity"),
-        },
-    ];
-    const { store: { pluginModal }, dispatch, } = useLocalData();
-    return (<>
-      <div className={`task-plugin-modal ${pluginModal ? "right-[3vw]" : "right-[-125vw]"}`}>
+  const [activeFilterTab, setActiveFilterTab] = useState("task");
+  const filterTabList = [
+    {
+      activeIndicator: activeFilterTab,
+      key: "account",
+      onClick: () => setActiveFilterTab("account"),
+    },
+    {
+      activeIndicator: activeFilterTab,
+      key: "contacts",
+      onClick: () => setActiveFilterTab("contacts"),
+    },
+    {
+      activeIndicator: activeFilterTab,
+      key: "opportunities",
+      onClick: () => setActiveFilterTab("opportunities"),
+    },
+    {
+      activeIndicator: activeFilterTab,
+      key: "task",
+      onClick: () => setActiveFilterTab("task"),
+    },
+    {
+      activeIndicator: activeFilterTab,
+      key: "activity",
+      onClick: () => setActiveFilterTab("activity"),
+    },
+  ];
+  const {
+    store: { pluginModal },
+    dispatch,
+  } = useLocalData();
+  return (
+    <>
+      <div className={`task-plugin-modal ${pluginModal ? "right-[3vw]" : "right-[-155vw]"}`}>
         <section className="w-full flex justify-center items-center relative py-4 px-5 border-b border-crm-gray-300">
           <section className="flex items-center gap-x-2">
             <button className="flex items-center">
-              <ArrowDownOutlinedIcon className="transform rotate-90" fill="#fff"/>
+              <ArrowDownOutlinedIcon className="transform rotate-90" fill="#fff" />
             </button>
             <span>1 of 4 task</span>
             <button className="flex items-center">
-              <ArrowDownOutlinedIcon className="transform rotate-270" fill="#fff"/>
+              <ArrowDownOutlinedIcon className="transform rotate-270" fill="#fff" />
             </button>
           </section>
           <button className="right-[20px] top-1/2 transform translate-y-[-50%] absolute">
-            <CloseIcon fill="#fff"/>
+            <CloseIcon fill="#fff" />
           </button>
         </section>
-        <FilterTab tabButton={filterTabList} className="px-4"/>
+        <FilterTab tabButton={filterTabList} className="px-4" />
 
         <section className="py-4 px-5 flex">
           <div>
@@ -67,7 +71,7 @@ export default function PluginTask() {
 
         <section className="flex  items-center gap-x-5 py-5 border-y border-crm-gray-300  px-3">
           <span className="text-white">To</span>
-          <MiniInput type="search" variant="crm-transparent" className="block"/>
+          <MiniInput type="search" variant="crm-transparent" className="block" />
           <span className="flex text-crm-gray">
             CC <span className="ml-3">Bcc</span>
           </span>
@@ -76,20 +80,28 @@ export default function PluginTask() {
           <label htmlFor="subject" className="whitespace-nowrap">
             Subject :
           </label>
-          <input type="text" placeholder="Your subject" className="border-0 outline-0 bg-transparent w-[98%]"/>
+          <input type="text" placeholder="Your subject" className="border-0 outline-0 bg-transparent w-[98%]" />
         </section>
         <TextEditor />
 
         <section className="py-4 border-t flex gap-x-4 border-crm-gray-300 px-5">
           <button className="py-2 px-4 rounded-lg border border-crm-gray-600">
-            <ClocklIcon fill="#AFAFAF"/>
+            <ClocklIcon fill="#AFAFAF" />
           </button>{" "}
           <Button variant="primary">Send & Complete</Button>
         </section>
       </div>
 
-      {pluginModal && (<div onClick={() => dispatch({
-                type: "HIDE_PLUGIN_MODAL",
-            })} className="fixed top-0 w-[100vw] duration-300 h-[100vh] opacity-90 bg-crm-dark"></div>)}
-    </>);
+      {pluginModal && (
+        <div
+          onClick={() =>
+            dispatch({
+              type: "HIDE_PLUGIN_MODAL",
+            })
+          }
+          className="fixed top-0 w-[100vw] duration-300 h-[100vh] opacity-90 bg-crm-dark"
+        ></div>
+      )}
+    </>
+  );
 }
--- src/pages/dashboard/schedules/index.js
+++ src/pages/dashboard/schedules/index.js
@@ -2,44 +2,51 @@ import Button from "@src/components/common/Button";
 import Dropdown from "@src/components/common/Dropdown";
 import MiniInput from "@src/components/common/MiniInput";
 import DashboardLayout from "@src/layout/dashboard-layout";
+import { useState } from "react";
+
 export default function Schedules() {
-    const timeZoneList = [
-        {
-            label: "GMT",
-            value: "GMT",
-        },
-    ];
-    const scheduleList = [
-        {
-            label: "12:00 AM",
-            value: "12:00 AM",
-        },
-        {
-            label: "01:00 PM",
-            value: "01:00 PM",
-        },
-        {
-            label: "02:00 PM",
-            value: "02:00 PM",
-        },
-        {
-            label: "03:00 PM",
-            value: "03:00 PM",
-        },
-    ];
-    return (<DashboardLayout>
+  const [mondaySchedule, setMondaySchedule] = useState({
+    start: "",
+    end: "",
+  });
+  const timeZoneList = [
+    {
+      label: "GMT",
+      value: "GMT",
+    },
+  ];
+  const scheduleList = [
+    {
+      label: "12:00 AM",
+      value: "12:00 AM",
+    },
+    {
+      label: "01:00 PM",
+      value: "01:00 PM",
+    },
+    {
+      label: "02:00 PM",
+      value: "02:00 PM",
+    },
+    {
+      label: "03:00 PM",
+      value: "03:00 PM",
+    },
+  ];
+  return (
+    <DashboardLayout>
       <div className="bg-crm-dark-300 py-7 px-4 md:px-8 text-white mt-8">
         <h4 className="text-[18px] font-semibold">Account Details</h4>
 
         <div className="flex flex-wrap flex-col md:flex-row gap-4 mt-5 ">
           <section>
             <label htmlFor="fullName">Full Name</label>
-            <MiniInput className="mt-2 block" placeholder="your full name" variant="crm-transparent" id="fullName" type="text"/>
+            <MiniInput className="mt-2 block" placeholder="your full name" variant="crm-transparent" id="fullName" type="text" />
           </section>
           <section className="">
             <label htmlFor="fullName">Select Time Zone</label>
 
-            <Dropdown variant="outlined" className="mt-2 min-w-[200px]" optList={timeZoneList}/>
+            <Dropdown variant="outlined" className="mt-2 min-w-[200px]" optList={timeZoneList} />
           </section>
 
           <Button variant="yellow" paddingClassName="py-3 px-6" className="font-normal md:self-end">
@@ -50,9 +57,31 @@ export default function Schedules() {
         <section className="flex flex-wrap flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Monday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px] " borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px] " borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Button variant="blue" paddingClassName="py-2 px-5">
+            <Dropdown
+              onChange={(e) => {
+                const currentSchedule = mondaySchedule;
+                console.log(e);
+                currentSchedule.start = e.current.value;
+                setMondaySchedule(currentSchedule);
+              }}
+              value={mondaySchedule.start}
+              variant="outlined"
+              className="min-w-[200px] "
+              borderVariantClass="border border-crm-gray-170"
+              optList={scheduleList}
+            />
+            <Dropdown
+              onChange={(e) => {
+                const currentSchedule = mondaySchedule;
+                currentSchedule.end = e.target.value;
+                setMondaySchedule(currentSchedule);
+              }}
+              variant="outlined"
+              className="min-w-[200px] "
+              borderVariantClass="border border-crm-gray-170"
+              optList={scheduleList}
+            />
+            <Button onClick={() => setMondaySchedule({ start: "12:00 AM", end: "12:00 AM" })} variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
           </div>
@@ -61,8 +90,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Tuesday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -72,8 +101,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Wednesday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -83,8 +112,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Thursday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -94,8 +123,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Friday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -105,8 +134,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Saturday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -116,8 +145,8 @@ export default function Schedules() {
         <section className="flex flex-col lg:flex-row lg:items-center gap-y-3 lg:w-[fit-content] rounded-lg gap-4 py-2 px-3 border border-crm-gray-300 mt-6 lg:min-w-[820px] lg:justify-between">
           <h5 className="text-white">Sunday : 12:00am - 12:00pm</h5>
           <div className="flex flex-wrap gap-4 flex-col md:flex-row">
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
-            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList}/>
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
+            <Dropdown variant="outlined" className="min-w-[200px]" borderVariantClass="border border-crm-gray-170" optList={scheduleList} />
             <Button variant="blue" paddingClassName="py-2 px-5">
               Clear
             </Button>
@@ -130,5 +159,6 @@ export default function Schedules() {
           </Button>
         </div>
       </div>
-    </DashboardLayout>);
+    </DashboardLayout>
+  );
 }
